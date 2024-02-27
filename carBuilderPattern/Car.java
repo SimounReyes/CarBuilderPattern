ca5f7cbdc19d3880f5741867a8748babd3bd7007
SimounReyes <Sigreyes313@gmail.com>
2024-02-27 12:04:36 +0800

    updating files

--- carBuilderPattern/Car.java
+++ carBuilderPattern/Car.java
@@ -1,11 +1,11 @@
-public class Car{
+public class Car {
     protected final String brand;
     protected final String engineType;
     protected final String transmission;
     protected final String color;
     protected final int passengerCapacity;
 
-    private Car(CarBuilder builder){
+    private Car(CarBuilder builder) {
         this.brand = builder.brand;
         this.engineType = builder.engineType;
         this.transmission = builder.transmission;
@@ -13,18 +13,61 @@ public class Car{
         this.passengerCapacity = builder.passengerCapacity;
     }
 
-    protected static class CarBuilder{
+    protected static class CarBuilder {
         private String brand;
         private String engineType;
         private String transmission;
         private String color;
         private int passengerCapacity;
 
-        public CarBuilder brand(String brand){
+        public CarBuilder brand(String brand) {
             this.brand = brand;
             return this;
         }
-        public CarBuilder
+
+        public CarBuilder engineType(String engineType) {
+            this.engineType = engineType;
+            return this;
+        }
+
+        public CarBuilder tranmission(String transmission) {
+            this.transmission = transmission;
+            return this;
+        }
+
+        public CarBuilder color(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public CarBuilder passengerCapacity(int passengerCapacity) {
+            this.passengerCapacity = passengerCapacity;
+            return this;
+        }
+
+        public Car build() {
+            return new Car(this);
+        }
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getEngineType() {
+        return engineType;
+    }
+
+    public String getTranmission() {
+        return transmission;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getPassengerCapacity() {
+        return passengerCapacity;
     }
 
 }
--- carBuilderPattern/CarAssemble.java
+++ carBuilderPattern/CarAssemble.java
@@ -1,1 +1,18 @@
+public class CarAssemble {
+    public static void main(String[] args) {
+        Car myCar = new Car.CarBuilder()
+                .color("Red")
+                .tranmission("Manual")
+                .brand("Nissan")
+                .engineType("gasoline")
+                .passengerCapacity(4)
+                .build();
 
+        System.out.println("You've successfully purchased a car!\n\nHere are the details of your car:");
+        System.out.println("Brand: " + myCar.getBrand());
+        System.out.println("Engine Type: " + myCar.getEngineType());
+        System.out.println("Transmission: " + myCar.getTranmission());
+        System.out.println("Color: " + myCar.getColor());
+        System.out.println("Passenger Capacity: " + myCar.getPassengerCapacity());
+    }
+}
